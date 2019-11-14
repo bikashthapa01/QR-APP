da909c46de67065f1042fd3f29c1c4510bb53fa2
Bikash <thapabikash48@gmail.com>
2019-11-14 18:38:40 +0530

    QR Code Reader and scanner

---
+++ app/src/main/java/net/smallacademy/qrapp/MainActivity.java
@@ -0,0 +1,61 @@
+package net.smallacademy.qrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+public class MainActivity extends AppCompatActivity {
+    EditText qrvalue;
+    Button generateBtn,scanBtn;
+    ImageView qrImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        qrvalue = findViewById(R.id.qrInput);
+        generateBtn = findViewById(R.id.generateBtn);
+        scanBtn = findViewById(R.id.scanBtn);
+        qrImage = findViewById(R.id.qrPlaceHolder);
+
+        generateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = qrvalue.getText().toString();
+                if(data.isEmpty()){
+                    qrvalue.setError("Value Required.");
+                }else {
+                    QRGEncoder qrgEncoder = new QRGEncoder(data,null, QRGContents.Type.TEXT,500);
+                    try {
+                        Bitmap qrBits = qrgEncoder.encodeAsBitmap();
+                        qrImage.setImageBitmap(qrBits);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Scanner.class));
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/net/smallacademy/qrapp/Scanner.java
@@ -0,0 +1,87 @@
+package net.smallacademy.qrapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class Scanner extends AppCompatActivity {
+    CodeScanner codeScanner;
+    CodeScannerView scannView;
+    TextView resultData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+        scannView = findViewById(R.id.scannerView);
+        codeScanner = new CodeScanner(this,scannView);
+        resultData = findViewById(R.id.resultsOfQr);
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultData.setText(result.getText());
+                    }
+                });
+
+            }
+        });
+
+
+        scannView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestForCamera();
+
+    }
+
+    public void requestForCamera() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                codeScanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(Scanner.this, "Camera Permission is Required.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+
+            }
+        }).check();
+    }
+
+
+}
